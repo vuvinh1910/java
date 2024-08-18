be4818a532479f3d23bda7f006964ad9511ecab7
vuvinh1910 <108977375+vuvinh1910@users.noreply.github.com>
2024-08-18 17:41:49 +0700

    Update hien thi loi chao.java

--- hien thi loi chao.java
+++ hien thi loi chao.java
@@ -6,3 +6,12 @@ public class Helloworld{
 }
 // chú ý : tên class chính phải trùng với tên file.java
 // vd: class helloworld thì tên file là helloworld.java
+
+public class HelloWorld
+{
+   public static void main(String[] args) 
+   {
+       System.out.println("125 + 256 = " + (125+256)); // hien thi ket hop chu va so ta dung "+", luu y lenh se thuc hien tu trai sang phai nen 
+                                                       // phai co dau ngoac () o 125+256
+   }
+}
