69e033bbdc79b103d1f779e5293485bfad494608
vuvinh1910 <108977375+vuvinh1910@users.noreply.github.com>
2024-08-18 17:32:21 +0700

    Create hien thi loi chao.java

---
+++ hien thi loi chao.java
@@ -0,0 +1,8 @@
+public class Helloworld{
+  public static void main (String[] args){
+    System.out.println("line1")
+    System.out.println("line2")
+  }
+}
+// chú ý : tên class chính phải trùng với tên file.java
+// vd: class helloworld thì tên file là helloworld.java
