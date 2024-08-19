05398f354f494c98c99ef33bbeb3a62bff799a4e
vuvinh1910 <108977375+vuvinh1910@users.noreply.github.com>
2024-08-19 17:33:32 +0700

    Create sap xep mang.java

---
+++ sap xep mang.java
@@ -0,0 +1,8 @@
+int[] numbers = {5, 2, 8, 1, 3};
+// Sắp xếp mảng theo thứ tự tăng dần
+Arrays.sort(numbers);
+
+Integer[] numbers = {5, 2, 8, 1, 3}; // Sử dụng Integer thay vì int vi sx giam gian yeu cau doi tuong
+// Sắp xếp mảng theo thứ tự giảm dần
+Arrays.sort(numbers, Collections.reverseOrder());
+
