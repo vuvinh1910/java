fe30f4b51d3b62bcb22e3e98483f2e14eb6c56aa
vuvinh1910 <108977375+vuvinh1910@users.noreply.github.com>
2024-08-19 16:53:04 +0700

    Create vong lap.java

---
+++ vong lap.java
@@ -0,0 +1,23 @@
+// for (khởi_tạo; điều_kiện; cập_nhật) {
+//     // Khối lệnh thực thi
+// }
+public class ForLoopExample {
+    public static void main(String[] args) {
+        for (int i = 1; i <= 5; i++) {
+            System.out.println("Lần lặp thứ: " + i);
+        }
+    }
+}
+// while (điều_kiện) {
+//     // Khối lệnh thực thi
+// }
+public class WhileLoopExample {
+    public static void main(String[] args) {
+        int i = 1;
+        while (i <= 5) {
+            System.out.println("Lần lặp thứ: " + i);
+            i++;
+        }
+    }
+}
+
