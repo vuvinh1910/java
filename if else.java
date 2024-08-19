ee6ee30b0c45e8e02117698a62677509b7b76be2
vuvinh1910 <108977375+vuvinh1910@users.noreply.github.com>
2024-08-19 16:20:19 +0700

    Create if else.java

---
+++ if else.java
@@ -0,0 +1,13 @@
+int number = 10;
+
+if (number > 0) {
+    System.out.println("Số là số dương.");
+}
+
+int number = -5;
+
+if (number > 0) {
+    System.out.println("Số là số dương.");
+} else {
+    System.out.println("Số không phải là số dương.");
+}
