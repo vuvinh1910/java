2de1b93a77b8e3c592c2d1f1c7e15e8a423b93a9
vuvinh1910 <108977375+vuvinh1910@users.noreply.github.com>
2024-08-19 16:47:51 +0700

    Create min max .java

---
+++ min max .java
@@ -0,0 +1,14 @@
+public class HelloWorld {
+  public static void main(String[] args) {
+      int a = 10;
+      int b = 20;
+
+      // Tìm giá trị nhỏ nhất
+      int min = Math.min(a, b);
+      System.out.println("Giá trị nhỏ nhất là: " + min);
+
+      // Tìm giá trị lớn nhất
+      int max = Math.max(a, b);
+      System.out.println("Giá trị lớn nhất là: " + max);
+  }
+}
