ff90cf685eb36959e96953883fbdbc4e7e5d8728
vuvinh1910 <108977375+vuvinh1910@users.noreply.github.com>
2024-08-23 17:48:37 +0700

    Create Entry.java

---
+++ OOP/package_learn/Entry.java
@@ -0,0 +1,9 @@
+package package_learn
+
+public class Entry{
+  public static void main(String[] args){
+    Student s = new Student();
+    s.name = "vinh";
+    System.out.println(s.name);
+  }
+}
