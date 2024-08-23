2d9f3dc8f54296a50be3a9c7605415f3dfaf265d
vuvinh1910 <108977375+vuvinh1910@users.noreply.github.com>
2024-08-23 19:41:27 +0700

    Create toan tu dieu kien.java

---
+++ toan tu dieu kien.java
@@ -0,0 +1,13 @@
+// condition ? valueIfTrue : valueIfFalse;
+
+public class Main {
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 20;
+
+        // Sử dụng toán tử điều kiện để tìm giá trị lớn hơn
+        int max = (a > b) ? a : b;
+
+        System.out.println("The maximum value is: " + max);
+    }
+}
