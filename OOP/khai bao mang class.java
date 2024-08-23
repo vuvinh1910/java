dada7b05c9d2764ecc10fab06094501d8353a50a
vuvinh1910 <108977375+vuvinh1910@users.noreply.github.com>
2024-08-23 18:48:40 +0700

    Update khai bao mang class.java

--- OOP/khai bao mang class.java
+++ OOP/khai bao mang class.java
@@ -2,5 +2,5 @@
 Student[] students = new Student[n];
 for (int i = 0; i < n; i++) {
 	// Khởi tạo các phần tử trong mảng students
-	students[i] = new Student();
+	students[i] = new Student(); // Trong Java, khi bạn muốn tạo một đối tượng của một lớp, bạn cần sử dụng từ khóa new để gọi constructor của lớp đó.
 }
