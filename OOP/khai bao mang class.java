b65a3e47481d1c019a120305f453721c0e7c75a0
vuvinh1910 <108977375+vuvinh1910@users.noreply.github.com>
2024-08-23 18:44:15 +0700

    Create khai bao mang class.java

---
+++ OOP/khai bao mang class.java
@@ -0,0 +1,6 @@
+// Khai báo mảng students với n phần tử
+Student[] students = new Student[n];
+for (int i = 0; i < n; i++) {
+	// Khởi tạo các phần tử trong mảng students
+	students[i] = new Student();
+}
