b07ce4c97e1f43a10ec2db911fb22452976df652
vuvinh1910 <108977375+vuvinh1910@users.noreply.github.com>
2024-08-24 18:52:44 +0700

    Create down-casting.java

---
+++ OOP/down-casting.java
@@ -0,0 +1,30 @@
+// Lớp cha
+class Animal {
+    public void makeSound() {
+        System.out.println("Animal makes a sound");
+    }
+}
+
+// Lớp con
+class Dog extends Animal {
+    private String breed;
+
+    public Dog(String breed) {
+        this.breed = breed;
+    }
+
+    public void fetch() {
+        System.out.println("Dog fetches the ball");
+    }
+
+    public String getBreed() {
+        return breed;
+    }
+}
+public class Main {
+    public static void main(String[] args) {
+        Animal animal = new Dog();
+        Dog dog = (Dog)animal;
+        dog.fetch();
+    }
+}
