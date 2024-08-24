d426ea820a0db65ba65e236ec4a4ed34e079d764
vuvinh1910 <108977375+vuvinh1910@users.noreply.github.com>
2024-08-24 18:41:03 +0700

    Create java.until.*.java

---
+++ java.until.*.java
@@ -0,0 +1,6 @@
+Nếu bạn viết import java.util.*;, điều này sẽ nhập toàn bộ các lớp và giao diện trong gói java.util, bao gồm:
+
+ArrayList, LinkedList, HashSet, HashMap, TreeMap, Stack, Queue, và nhiều lớp khác về cấu trúc dữ liệu.
+Date, Calendar, TimeZone cho việc xử lý ngày tháng và thời gian.
+Random cho việc tạo số ngẫu nhiên.
+Scanner cho việc nhập liệu từ bàn phím.
