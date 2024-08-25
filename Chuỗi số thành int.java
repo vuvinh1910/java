46216c073bf7d70b36d4bb252735194952f40250
vuvinh1910 <108977375+vuvinh1910@users.noreply.github.com>
2024-08-25 22:02:54 +0700

    Create Chuỗi số thành int.java

---
+++ Chuỗi số thành int.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        String numberString = "0080"; // Chuỗi số với tiền tố 0
+        int number = Integer.parseInt(numberString); // Chuyển đổi chuỗi thành số nguyên
+
+        System.out.println("Number: " + number); // In kết quả
+    }
+}
