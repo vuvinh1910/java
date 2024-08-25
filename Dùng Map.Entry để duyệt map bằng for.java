cfd06335a3385a0672590cae8f738e1abe00d7b3
vuvinh1910 <108977375+vuvinh1910@users.noreply.github.com>
2024-08-25 20:31:39 +0700

    Create Dùng Map.Entry để duyệt map bằng for.java

---
+++ Dùng Map.Entry để duyệt map bằng for.java
@@ -0,0 +1,18 @@
+import java.util.HashMap;
+import java.util.Map;
+
+public class MapTraversalExample {
+    public static void main(String[] args) {
+        Map<String, String> map = new HashMap<>();
+        map.put("key1", "value1");
+        map.put("key2", "value2");
+        map.put("key3", "value3");
+
+        // Duyệt qua các mục của Map
+        for (Map.Entry<String, String> entry : map.entrySet()) {
+            String key = entry.getKey();
+            String value = entry.getValue();
+            System.out.println("Key: " + key + ", Value: " + value);
+        }
+    }
+}
