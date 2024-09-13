1de83456ba005b72c97cc2b30f9776ae573e13ce
vuvinh1910 <108977375+vuvinh1910@users.noreply.github.com>
2024-09-13 18:31:36 +0700

    Create them so 0.java

---
+++ them so 0.java
@@ -0,0 +1,19 @@
+public class Quiz {
+    public static void main(String[] args) {
+        // In số 5 thành 0005
+        System.out.println(String.format("%04d", 5)); // 0005
+
+        // In số 10 thành 0010
+        System.out.println(String.format("%04d", 10)); // 0010
+    }
+}
+
+public class Quiz {
+    public static void main(String[] args) {
+        // In số 5 thành 0005
+        System.out.printf("%04d\n", 5); // 0005
+
+        // In số 10 thành 0010
+        System.out.printf("%04d\n", 10); // 0010
+    }
+}
