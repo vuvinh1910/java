71eca8d77277b4001dddb49107dd08b374e9bd5f
vuvinh1910 <108977375+vuvinh1910@users.noreply.github.com>
2024-09-13 19:16:13 +0700

    Update file.java

--- file.java
+++ file.java
@@ -7,8 +7,13 @@ File file = new File("DATA.in"); // duong dan hien tai chua file java
   File x = new File("DATA.in");
     try{
         Scanner sc = new Scanner(x);
-        while(sc.hasNextLine()){ // hoặc hasNext, dùng để kiểm tra xem có dòng , dữ liệu tiếp theo trong file hay không. 
+        while(sc.hasNextLine()){ // dùng để kiểm tra xem có dòng còn trong file hay không. 
             System.out.println(sc.nextLine()); // in ra từng dòng dữ liệu trong file
+        }
+
+      while(sc.hasNext()){ // ktra dữ liệu tiếp theo có tồn tại trong file hay không. 
+            System.out.println(sc.next()); // in ra từng dữ liệu trong file: vd: trong file có chục dòng, chục khoảng trống lớn nỏ và có 2 dữ liệu văn bản "vinh" và "19"
+            // thí sẽ in ra "vinh" và "19" bỏ qua mọi khoảng cách và dòng trống.
         }
     }catch(FileNotFoundException e){ // kiểm tra xem có tồn tại file hay không.
   
