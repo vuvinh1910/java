91188c76b00bea262dee07d8801dd8846d01a3be
vuvinh1910 <108977375+vuvinh1910@users.noreply.github.com>
2024-09-13 20:56:57 +0700

    Create chuyen co so.java

---
+++ chuyen co so.java
@@ -0,0 +1,7 @@
+String binaryString = "1010"; // Chuỗi nhị phân cần chuyển đổi
+// Chuyển đổi từ chuỗi nhị phân sang số nguyên
+int number = Integer.parseInt(binaryString, 2);
+
+int number = 10; // Số nguyên cần chuyển đổi
+// Chuyển đổi từ số nguyên sang chuỗi nhị phân
+String binaryString = Integer.toBinaryString(number);
