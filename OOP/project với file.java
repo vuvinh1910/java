7431b1fffcccdba047580c656af91193119413c9
vuvinh1910 <108977375+vuvinh1910@users.noreply.github.com>
2024-09-14 15:42:34 +0700

    Update project với file.java

--- OOP/project với file.java
+++ OOP/project với file.java
@@ -1,4 +1,5 @@
-package project;
+// có project tên là javaNet gồm folder thư mục con tạo bằng netbean.
+// nếu file ở trong javaNet
 File file = new File("tên file");
-// nếu file trong thư mục con của project thì:
+// nếu file trong thư mục con của javaNet thì:
 File file = new File("Thư mục con/Tên file");
