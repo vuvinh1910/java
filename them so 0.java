d775e47b79916f79afb9104a19a6028fb12252ae
vuvinh1910 <108977375+vuvinh1910@users.noreply.github.com>
2024-09-15 20:37:40 +0700

    Update them so 0.java

--- them so 0.java
+++ them so 0.java
@@ -1,7 +1,7 @@
 public class Quiz {
     public static void main(String[] args) {
         // In số 5 thành 0005
-        System.out.println(String.format("%04d", 5)); // 0005
+        System.out.println(String.format("MH%04d", 5)); // MH0005
 
         // In số 10 thành 0010
         System.out.println(String.format("%04d", 10)); // 0010
