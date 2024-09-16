88df0e44c4acfbfbcb939cc493432db612fa02e2
vuvinh1910 <108977375+vuvinh1910@users.noreply.github.com>
2024-09-16 22:32:56 +0700

    Create lam tron so sau dau phay.java

---
+++ lam tron so sau dau phay.java
@@ -0,0 +1,5 @@
+gpa = 7.11116500000;
+String.format("%.5f", gpa); // lam tron 5 chu so sau dau phay thanh 7.11117;
+// trong mot so truong hop tinh toan lam tron sau dau phay sai vd: 7.65 thanh 7.6 ma khong phai 7.7
+// do la do 7.65 thuc ra la 7.6499999 nhung may tinh hien thi 7.65
+// khac phuc bang cach + them 0.00001
